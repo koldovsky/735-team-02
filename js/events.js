5753772218bb93842979f1e746bd36c7b3b349e7
Ievgen Hnatiuk <121964936+eugenehnatiuk@users.noreply.github.com>
2023-01-29 19:07:03 +0000

    asfaf

--- js/events.js
+++ js/events.js
@@ -1,6 +1,11 @@
-(function (){
+(function () {
+    function getTitleMessage() {
+      const titleMessages = ["Our Bear Events", "Stay closer", "Stay tuned!"];
+      return titleMessages[Math.floor(Math.random() * titleMessages.length)];
+    } 
     const headerH2 = document.querySelector(".our-events__title");
-    headerH2.innerText = "Our Bear Events";
+    headerH2.innerText = getTitleMessage();
+  })();
 
 
-})()
+
---
+++ js/events_timer.js
@@ -0,0 +1,49 @@
+(function () {
+    const timer = document.querySelector(".open-day__side__clock");
+    timer.innerText = ''
+    
+    const x = setInterval(function () {
+      const endTime = new Date("2023-03-27T23:59:59");
+      const startTime = new Date().getTime('uk');
+      const distance = endTime - startTime;
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        if (distance < 0) {
+        clearInterval(x);
+      }
+        timer.innerText =
+        days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  
+      
+    }, 1000);
+   
+    })();
+
+    (function(){
+      const timer = document.querySelector(".festival__side__clock");
+    timer.innerText = ''
+    
+    const x = setInterval(function () {
+      const endTime = new Date("2023-04-27T23:59:59");
+      const startTime = new Date().getTime();
+      const distance = endTime - startTime;
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        if (distance < 0) {
+        clearInterval(x);
+      }
+        timer.innerText =
+        days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  
+      
+    }, 1000);
+    })()
+  
