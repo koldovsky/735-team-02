df0121fb140e5ec9232dc261f8dbec554b0a9752
Bohdan Olesko <89656344+KIBA659@users.noreply.github.com>
2023-02-01 18:09:16 +0000

    add carousel js+css

--- js/photo-carousel.js
+++ js/photo-carousel.js
@@ -1,29 +1,31 @@
 (function () {
     const slides = [
-        '<div class="slide__container__carousel"><img class="slides" src="img/four_woman_with_beer.png"></div>',
-        '<div class="slide__container__carousel"><img class="slides" src="img/breaks_beer.png"></div>',
-        '<div class="slide__container__carousel"><img class="slides" src="img/pour_beer.png"></div>',
-        '<div class="slide__container__carousel"><img class="slides" src="img/man_drink_beer_.png"></div>',
+        '<div class="slide__container__carousel "><img class="slides carousel_1" src="img/carousel_1.png"></div>',
+        '<div class="slide__container__carousel "><img class="slides carousel_2" src="img/carousel_2.png"></div>',
+        '<div class="slide__container__carousel "><img class="slides carousel_3" src="img/carousel_3.png"></div>',
+        '<div class="slide__container__carousel "><img class="slides carousel_4" src="img/carousel_4.png"></div>',
+        '<div class="slide__container__carousel "><img class="slides carousel_5" src="img/carousel_5.png"></div>',
     ]
 
     let currentSlideIndex = 0;
 
     function renderslide() { 
-        const slidescontainer = document.querySelector('.gallery__container')
-        slidescontainer.innerHTML = slides[currentSlideIndex]
-            if(window.innerWidth >= 768){
-                const secondslidIdx = currentSlideIndex + 1 >= slides.length ? 0 : currentSlideIndex + 1
-                slidescontainer.innerHTML += slides[secondslidIdx];
-                    if(window.innerWidth >= 1000){
-                        const thirddslidIdx = secondslidIdx + 1>= slides.length ? 0 : secondslidIdx + 1 
-                        slidescontainer.innerHTML += slides[thirddslidIdx];
-                        if(window.innerWidth >= 1300){
-                            const fourdslidIdx = thirddslidIdx + 1>= slides.length ? 0 :thirddslidIdx + 1 
-                            slidescontainer.innerHTML += slides[fourdslidIdx];
-                        }
+        if(window.innerWidth >= 160){
+            const slidescontainer = document.querySelector('.gallery__container')
+            slidescontainer.innerHTML = slides[currentSlideIndex]
+                if(window.innerWidth >= 768){
+                    const secondslidIdx = currentSlideIndex + 1 >= slides.length ? 0 : currentSlideIndex + 1
+                    slidescontainer.innerHTML += slides[secondslidIdx];
+                        if(window.innerWidth >= 1000){
+                            const thirddslidIdx = secondslidIdx + 1>= slides.length ? 0 : secondslidIdx + 1 
+                            slidescontainer.innerHTML += slides[thirddslidIdx];
+                            if(window.innerWidth >= 1300){
+                                const fourdslidIdx = thirddslidIdx + 1>= slides.length ? 0 :thirddslidIdx + 1 
+                                slidescontainer.innerHTML += slides[fourdslidIdx];
+                            }
                     }
-            }
-
+                }
+        }
     }
     window.addEventListener('resize',renderslide);
 
@@ -42,11 +44,18 @@
     const btnNext = document.querySelector('.button__gallery__next');
     btnNext.addEventListener('click',nextslide);
 
-    const btnPrev = document.querySelector('.button__gallery__prew');
+    const btnPrev = document.querySelector('.button__gallery__prev');
     btnPrev.addEventListener('click',() => {
         currentSlideIndex =  currentSlideIndex - 1 < 0 ? slides.length - 1 : currentSlideIndex - 1;
         renderslide();
     })
-
-    
+})();
+(function(){
+    function zoom(){
+        document.querySelectorAll('.carousel_1 , .carousel_2 , .carousel_3 , .carousel_4, .carousel_5').forEach(item => {
+            item.addEventListener('mouseover', event => {
+                item.classList.add('opened')
+            } )
+        }) }
+    setInterval(zoom,1);
 })();
