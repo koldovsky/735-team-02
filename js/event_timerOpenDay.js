917b88adac216578775665121e4d4eed20095f81
Ievgen Hnatiuk <121964936+eugenehnatiuk@users.noreply.github.com>
2023-02-01 14:59:55 +0000

    events carousel

---
+++ js/event_timerOpenDay.js
@@ -0,0 +1,22 @@
+(function () {
+  const timer = document.querySelector(".open-day__side__clock");
+  timer.innerText = ''
+
+  const x = setInterval(function () {
+    const endTime = new Date("2023-08-19T23:59:59");
+    const startTime = new Date().getTime('uk');
+    const distance = endTime - startTime;
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    if (distance < 0) {
+      clearInterval(x);
+    }
+    timer.innerText = "Days left: " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+
+  }, 1000);
+})()
---
+++ js/events-carousel.js
@@ -0,0 +1,74 @@
+(function () {
+
+    // Створити об'єкти Events
+    const events = [
+      {
+        id: "open-day",
+        eventTitle: "OPEN DAY",
+        eventPrice: "Free",
+        eventDate: "AUGUST 19, 2023",
+        eventText:
+          "Do you want to know all the secrets of brewing? Want to understand how real craft beer is made? We will tell you, show you, and offer a free tasting.",
+        button: "Book",
+        eventImg: "img/our_events_bear_and_burrel.png",
+        timer: "open-day__side__clock"
+      },
+      {
+        id: "festival",
+        eventTitle: "AUTUMN BEER FESTIVAL",
+        eventPrice: "From $80",
+        eventDate: "SEPTEMBER 01, 2023",
+        eventText:
+          "Do you want to not only enjoy beer but also have fun? We're holding a festival! The best beers, live music and fantastic atmosphere are waiting!",
+        button: "Book",
+        eventImg: "img/autumn_bear_festival.png",
+        timer: "festival__side__clock"
+      },
+    ];
+
+    // Заповнення масиву для подальшого відображення слайдів
+    const slidesArray = [];
+    function arraySlides(events) {
+      for (const event of events) {
+        slidesArray.push(`<div class="schedule__open-day open-day ${event.id}">
+              <div class="open-day__left-side">
+                      <img class="open-day__image" src="${event.eventImg}"
+                          alt="${event.eventTitle}">
+              </div>
+                  <div class="open-day__right-side side">
+                      <div class="side__content">
+                          <p class="event-price">${event.eventPrice}</p>
+                          <h3 class="event-title">${event.eventTitle}</h3>
+                          <p class="${event.timer}"></p>
+                          <p class="event-date">${event.eventDate}</p>
+                          <p class="event-text">${event.eventText}</p>
+                          <a href="#footer" class="schedule-button">${event.button}</a>
+                      </div>
+                      
+                  </div>
+          </div>`);
+      }
+      return slidesArray;
+    }
+    arraySlides(events);
+  
+    // Створення слайдів з масиву
+    let currentSlideIdx = 0;
+  
+    function renderCarousel() {
+      const carouselContainer = document.querySelector(".carousel");
+      carouselContainer.innerHTML = slidesArray[currentSlideIdx];
+    }
+    function nextSlide() {
+      currentSlideIdx =
+        currentSlideIdx + 1 >= slidesArray.length
+          ? (currentSlideIdx = 0)
+          : currentSlideIdx + 1;
+      renderCarousel();
+    }
+    setInterval(nextSlide, 5000);
+    nextSlide();
+
+  
+  })();
+  
